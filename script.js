e7c87e64e838da6778fec97cbabc7be34088c6ce
Kushal Joshi <143593709+codingwithkushal@users.noreply.github.com>
2024-02-11 23:33:50 +0530

    Update script.js
    added javascript for the copy button

--- script.js
+++ script.js
@@ -2,3 +2,22 @@ function generateBoxCode() {
     var code = Math.floor(1000 + Math.random() * 9000);
     document.getElementById("boxCode").innerText = code;
 }
+
+function copyCode() {
+    var codeElement = document.getElementById("boxCode");
+    var code = codeElement.innerText;
+    
+    var textarea = document.createElement("textarea");
+    textarea.value = code;
+    document.body.appendChild(textarea);
+    
+    textarea.select();
+    textarea.setSelectionR
+    
+    document.execCommand("copy");
+    
+    
+    document.body.removeChild(textarea);
+    
+    alert("Code copied to clipboard: " + code);
+}
